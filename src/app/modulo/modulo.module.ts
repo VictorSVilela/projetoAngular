d02931490dae5496017719ae8dfe4fc594fa4f76
Victor Soares Vilela <victor_svg@yahoo.com.br>
2021-11-16 20:30:39 -0300

    Atividade para criar projeto Angular

---
+++ src/app/modulo/modulo.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModuloRoutingModule } from './modulo-routing.module';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+
+
+
+@NgModule({
+  declarations: [
+    C1Component,
+    C2Component,
+    C3Component
+  ],
+  imports: [
+    CommonModule,
+    ModuloRoutingModule
+  ]
+})
+export class ModuloModule { }
